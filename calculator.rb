1915dd1976097d559a06149809d487efcae7a9a3
Dylan <dylanlemayisokay@gmail.com>
2017-07-20 19:08:01 -0700

    First method, gets user's name

---
+++ calculator.rb
@@ -0,0 +1,12 @@
+# This method greets the user and request thier name.
+# It will also return their name
+
+def greeting
+
+	name = gets "Hello there! What is you name?"
+
+	puts "Hello #{name}, it's great to meet you. I'm excited to do some calculations for you!"
+
+	return name
+
+end

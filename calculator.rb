af3b1f7f078b2dd56a6dc584654425f29a0e8684
Dylan <dylanlemayisokay@gmail.com>
2017-07-20 19:23:40 -0700

    Adds method for determining calculation type

--- calculator.rb
+++ calculator.rb
@@ -10,3 +10,25 @@ def greeting
 	return name
 
 end
+
+# This method asks the user which calculation they would like to make.
+# It will return the operation or an display and error if the user enters anything other than a valid selection
+
+def request_calulation_type (calculation_selection, a , b)
+
+	puts "Type 1 to add, 2 to subtract, 3 to multiply, or 4 to divide:"
+
+	calulation_selection = gets.to_i
+
+	if calculation_selection == 1
+		return "add"
+	elsif calculation_selection == 2
+		return "subtract"
+	elsif calculation_selection == 3
+		return "multiply"
+	elsif calculation_selection == 4
+		return "divide"
+	else 
+		return "Error"
+	end
+end
